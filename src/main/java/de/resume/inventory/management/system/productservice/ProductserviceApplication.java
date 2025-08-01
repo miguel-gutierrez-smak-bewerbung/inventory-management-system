74e216bc517ea845bef9ccd7af5db5eedc87054a
Miguel Gutierrez <miguel-gutierrez@mail.de>
2025-08-01 13:29:20 +0200

    initial commit

---
+++ src/main/java/de/resume/inventory/management/system/productservice/ProductserviceApplication.java
@@ -0,0 +1,13 @@
+package de.resume.inventory.management.system.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductserviceApplication.class, args);
+    }
+
+}
