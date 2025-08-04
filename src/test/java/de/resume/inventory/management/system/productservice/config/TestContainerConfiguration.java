f7ec01e5188a82653d518d910c1d44a3f0d5ec6d
Miguel Gutierrez <miguel-gutierrez@mail.de>
2025-08-04 14:14:26 +0200

    issue-#2-refactor: update TestContainer configuration and clean up ProductRepositoryTest
    - Refactor `TestContainerConfiguration` to use `@Container` for PostgreSQL and register dynamic properties.
    - Remove redundant `@Testcontainers` annotation from `ProductRepositoryTest`.

--- src/test/java/de/resume/inventory/management/system/productservice/config/TestContainerConfiguration.java
+++ src/test/java/de/resume/inventory/management/system/productservice/config/TestContainerConfiguration.java
@@ -1,17 +1,26 @@
 package de.resume.inventory.management.system.productservice.config;
 
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@TestConfiguration(proxyBeanMethods = false)
+@TestConfiguration
+@Testcontainers
 public class TestContainerConfiguration {
 
-    @Bean
-    PostgreSQLContainer<?> postgreSQLContainer() {
-        return new PostgreSQLContainer<>("postgres:16-alpine")
-                .withDatabaseName("product-service")
-                .withUsername("postgres")
-                .withPassword("test");
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine")
+        .withDatabaseName("testdb")
+        .withUsername("test")
+        .withPassword("test");
+
+    @DynamicPropertySource
+    static void overrideProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
     }
 }
--- src/test/java/de/resume/inventory/management/system/productservice/repositories/ProductRepositoryTest.java
+++ src/test/java/de/resume/inventory/management/system/productservice/repositories/ProductRepositoryTest.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.math.BigDecimal;
 import java.util.Optional;
@@ -18,7 +17,6 @@ import java.util.Optional;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
-@Testcontainers
 @Import(TestContainerConfiguration.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ProductRepositoryTest {
