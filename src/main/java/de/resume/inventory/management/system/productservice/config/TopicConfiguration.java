55ba5a40f545dafe07d1e6bc7d5336e0be467b46
Miguel Gutierrez <miguel-gutierrez@mail.de>
2025-08-04 21:26:44 +0200

    issue-#4: add topic configuration for Kafka in ProductService
    - Update `application-local.yml` to include `product-upsert` and `product-upsert-fail` topics.
    - Introduce `TopicConfiguration` class to manage topic properties.

---
+++ src/main/java/de/resume/inventory/management/system/productservice/config/TopicConfiguration.java
@@ -0,0 +1,15 @@
+package de.resume.inventory.management.system.productservice.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "topics")
+public class TopicConfiguration {
+    private String productUpsert;
+    private String productUpsertFail;
+}
