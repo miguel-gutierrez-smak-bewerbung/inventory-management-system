a303a3f14f5431e3a97254329370cae87940e2f2
Miguel Gutierrez <miguel-gutierrez@mail.de>
2025-08-05 19:58:06 +0200

    issue-#4: enhance ProductValidationServiceTest and update dependencies
    - Add `validateProductToCreate` test method in `ProductValidationServiceTest` to verify product creation validation logic.
    - Include `junit-jupiter` dependency in `pom.xml` for unit testing with JUnit 5.

--- src/test/java/de/resume/inventory/management/system/productservice/services/validation/ProductValidationServiceTest.java
+++ src/test/java/de/resume/inventory/management/system/productservice/services/validation/ProductValidationServiceTest.java
@@ -1,6 +1,22 @@
 package de.resume.inventory.management.system.productservice.services.validation;
 
-public class ProductValidationServiceTest {
 
-    ProductValidationService sut = new ProductValidationServiceImpl();
+import de.resume.inventory.management.system.productservice.models.dtos.ProductToCreateDto;
+import de.resume.inventory.management.system.productservice.models.enums.Category;
+import de.resume.inventory.management.system.productservice.models.enums.Unit;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ProductValidationServiceTest {
+
+    private final ProductValidationService sut = new ProductValidationServiceImpl();
+
+    @Test
+    void validateProductToCreate() {
+        final ProductToCreateDto productToCreateDto = new ProductToCreateDto(
+                "validName", "PRD-2024-0812", null, Category.ELECTRONICS, Unit.PIECE,2.50
+        );
+
+        Assertions.assertDoesNotThrow(() -> sut.validateProductToCreate(productToCreateDto));
+    }
 }
