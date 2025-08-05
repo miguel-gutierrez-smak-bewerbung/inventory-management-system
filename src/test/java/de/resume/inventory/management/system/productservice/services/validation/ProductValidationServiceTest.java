a697650a3b5de7854449c9753699c1938f3a8c34
Miguel Gutierrez <miguel-gutierrez@mail.de>
2025-08-05 19:41:55 +0200

    issue-#4: add ProductValidationServiceTest class for unit testing

---
+++ src/test/java/de/resume/inventory/management/system/productservice/services/validation/ProductValidationServiceTest.java
@@ -0,0 +1,6 @@
+package de.resume.inventory.management.system.productservice.services.validation;
+
+public class ProductValidationServiceTest {
+
+    ProductValidationService sut = new ProductValidationServiceImpl();
+}
